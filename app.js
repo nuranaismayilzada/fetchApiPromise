76247c02b8ef924ba0255dc1b6c58efbc20e102a
“[ilvintagiyev]” <[email>
2024-04-19 12:36:52 +0400

    buttons added table

--- app.js
+++ app.js
@@ -37,11 +37,28 @@ fetch('https://northwind.vercel.app/api/products').then(res=>res.json())
         let tdName=document.createElement("td")
         let country=document.createElement("td")
         let price=document.createElement("td")
+        let tdDanger=document.createElement("td")
+        let tdWarning=document.createElement("td")
+        let tdSuccess=document.createElement("td")
+        let deleteButton=document.createElement("button")
+        let successButton=document.createElement("button")
+        let editButton=document.createElement("button")
+        deleteButton.className="btn btn-danger";
+        deleteButton.innerText="Delete"
+        deleteButton.style.backgroundColor="red"
+        successButton.className="btn btn-success"
+        successButton.innerText="Detail"
+        editButton.className="btn btn-warning";
+        editButton.style.backgroundColor="yellow";
+        editButton.innerText="Edit"
         tdName.innerText=elem.name
         id.innerText=elem.id
         price.innerText=elem.unitPrice
         country.innerText=elem.name
-        tr.append(id,tdName,price,country)
+        tdDanger.append(deleteButton)
+        tdWarning.append(editButton)
+        tdSuccess.append(successButton)
+        tr.append(id,tdName,price,country,tdDanger,tdWarning,tdSuccess)
         tbody.append(tr)
 
     });
