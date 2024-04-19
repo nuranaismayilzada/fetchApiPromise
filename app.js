719aaad72b29b4dfb26baa63c8a28b00dbac4fd2
“[ilvintagiyev]” <[email>
2024-04-19 13:22:53 +0400

    commit

--- app.js
+++ app.js
@@ -28,38 +28,61 @@
 //         console.log(item);
 //     })
 // })
-let tbody=document.querySelector(".bodyy")
-fetch('https://northwind.vercel.app/api/products').then(res=>res.json())
-.then(data=>{
-    data.forEach(elem => {
-        let tr=document.createElement("tr") 
-        let id=document.createElement("td")
-        let tdName=document.createElement("td")
-        let country=document.createElement("td")
-        let price=document.createElement("td")
-        let tdDanger=document.createElement("td")
-        let tdWarning=document.createElement("td")
-        let tdSuccess=document.createElement("td")
-        let deleteButton=document.createElement("button")
-        let successButton=document.createElement("button")
-        let editButton=document.createElement("button")
-        deleteButton.className="btn btn-danger";
-        deleteButton.innerText="Delete"
-        deleteButton.style.backgroundColor="red"
-        successButton.className="btn btn-success"
-        successButton.innerText="Detail"
-        editButton.className="btn btn-warning";
-        editButton.style.backgroundColor="yellow";
-        editButton.innerText="Edit"
-        tdName.innerText=elem.name
-        id.innerText=elem.id
-        price.innerText=elem.unitPrice
-        country.innerText=elem.name
-        tdDanger.append(deleteButton)
-        tdWarning.append(editButton)
-        tdSuccess.append(successButton)
-        tr.append(id,tdName,price,country,tdDanger,tdWarning,tdSuccess)
-        tbody.append(tr)
+let tbody = document.querySelector(".bodyy")
+let modal=document.querySelector(".modal-body")
+fetch('https://northwind.vercel.app/api/products').then(res => res.json())
+    .then(data => {
+        data.forEach(elem => {
 
-    });
-})
+            let tr = document.createElement("tr")
+            let id = document.createElement("td")
+            let tdName = document.createElement("td")
+            let country = document.createElement("td")
+            let price = document.createElement("td")
+            let tdDanger = document.createElement("td")
+            let tdWarning = document.createElement("td")
+            let tdSuccess = document.createElement("td")
+            let deleteButton = document.createElement("button")
+            let successButton = document.createElement("button")
+            let editButton = document.createElement("button")
+
+            deleteButton.className = "btn btn-danger";
+            deleteButton.innerText = "Delete"
+            deleteButton.style.backgroundColor = "red"
+            successButton.className = "btn btn-success"
+            successButton.innerText = "Detail"
+            editButton.className = "btn btn-warning";
+            editButton.style.backgroundColor = "yellow";
+            editButton.innerText = "Edit"
+            tdName.innerText = elem.name
+            id.innerText = elem.id
+            price.innerText = elem.unitPrice
+            country.innerText = elem.name
+
+
+            deleteButton.addEventListener("click", (e) => {
+                // fetch('https://northwind.vercel.app/api/products' + '/' + elem.id, {
+                //     method: 'delete'
+                // })
+                //     .then(response => response.json());
+                e.target.parentElement.parentElement.remove();
+            })
+            
+            tdWarning.addEventListener("click",()=>{
+                tdWarning.setAttribute("data-bs-toggle","modal")
+                tdWarning.setAttribute("data-bs-target","#exampleModal")
+                // console.log(modal.innerText);
+                modal.innerText=elem.id
+            })
+
+
+
+
+            tdDanger.append(deleteButton)
+            tdWarning.append(editButton)
+            tdSuccess.append(successButton)
+            tr.append(id, tdName, price, country, tdDanger, tdWarning, tdSuccess)
+            tbody.append(tr)
+
+        });
+    })
